fb4f32a4e9a185f0363bc702eceb04f6dafd29b2
wangyesheng <1270230529@qq.com>
2023-01-15 22:04:34 +0800

    feat: first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <view class="content">
+    <u-button type="primary" text="Hello uView"></u-button>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello",
+    };
+  },
+  onLoad() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+.content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background: $uni-bg-color-grey;
+}
+</style>
