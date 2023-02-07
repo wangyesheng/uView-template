1e099b232fa0c9a216404d41648326994daed35e
wangyesheng <1270230529@qq.com>
2023-02-07 23:43:42 +0800

    feat: add demo

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,19 +1,11 @@
 <template>
   <view class="content">
-    <view class="dropdown">
-      <u-dropdown>
-        <u-dropdown-item
-          v-model="value1"
-          title="距离"
-          :options="options1"
-        ></u-dropdown-item>
-        <u-dropdown-item
-          v-model="value2"
-          title="温度"
-          :options="options2"
-        ></u-dropdown-item>
-      </u-dropdown>
+    <image class="logo" src="/static/uview/common/logo.png"></image>
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
     </view>
+    <u-button type="primary" @click="onNav">跳转</u-button>
+    <u-tabbar :list="tabbar" :mid-button="true"></u-tabbar>
   </view>
 </template>
 
@@ -21,40 +13,73 @@
 export default {
   data() {
     return {
-      value1: 1,
-      value2: 2,
-      options1: [
-        {
-          label: "默认排序",
-          value: 1,
-        },
-        {
-          label: "距离优先",
-          value: 2,
-        },
-        {
-          label: "价格优先",
-          value: 3,
-        },
-      ],
-      options2: [
-        {
-          label: "去冰",
-          value: 1,
-        },
-        {
-          label: "加冰",
-          value: 2,
-        },
-      ],
+      title: "Hello uView",
+      tabbar: "",
     };
   },
-  onLoad() {},
-  methods: {},
+  onLoad() {
+    /**
+     * 示例中为每个tabbar页面都写了一遍tabbar变量，您可以将tabbar数组写入到vuex中，这样可以全局引用
+     */
+    this.tabbar = [
+      {
+        iconPath: "/static/uview/example/component.png",
+        selectedIconPath: "/static/uview/example/component_select.png",
+        text: "组件",
+        count: 2,
+        isDot: true,
+        pagePath: "/pages/index/index",
+      },
+      {
+        iconPath: "/static/uview/example/js.png",
+        selectedIconPath: "/static/uview/example/js_select.png",
+        text: "工具",
+        midButton: true,
+        pagePath: "/pages/js/index",
+      },
+      {
+        iconPath: "/static/uview/example/template.png",
+        selectedIconPath: "/static/uview/example/template_select.png",
+        text: "模板",
+        pagePath: "/pages/template/index",
+      },
+    ];
+  },
+
+  methods: {
+    onNav() {
+      uni.navigateTo({
+        url: "/pages/me/index",
+      });
+    },
+  },
 };
 </script>
 
-<style lang="scss">
+<style>
 .content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
 }
 </style>
---
+++ src/pages/me/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="me-wrap">我的</div>
+</template>
