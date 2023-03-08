8a124badd6c0ba6eddd95cbcb433596cd88cebc1
wangyesheng <1270230529@qq.com>
2023-03-08 10:38:20 +0800

    fix:update

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,11 +1,12 @@
 <style lang="scss" scoped>
 .header {
-  height: 540rpx;
   background: linear-gradient(
-    180deg,
-    #148ddc 0%,
-    #67c8f9 40%,
-    rgba(86, 255, 174, 0) 100%
+    189deg,
+    #2398e1 0%,
+    #8ee2e9 33%,
+    rgba(213, 236, 221, 0.27) 54%,
+    rgba(207, 230, 222, 0) 100%,
+    rgba(216, 207, 230, 0) 100%
   );
   padding: 0 30rpx;
   box-sizing: border-box;
@@ -14,204 +15,145 @@
     height: 100%;
     display: flex;
     flex-direction: column;
-    justify-content: flex-end;
-    ::v-deep {
-      .u-swiper-wrap {
-        border-radius: 24rpx !important;
+
+    &_title {
+      font-size: 44rpx;
+      font-weight: bold;
+      color: #252828;
+
+      p {
+        margin-bottom: 20rpx;
+        width: 176rpx;
+        position: relative;
+        z-index: 1;
+
+        &::after {
+          content: "";
+          position: absolute;
+          left: 0;
+          bottom: 0;
+          width: 100%;
+          height: 14rpx;
+          background: #c3eeeb;
+          z-index: -1;
+        }
       }
     }
-    .banner {
-      height: 338rpx;
-      border-radius: 24rpx;
+
+    .select-wrap {
+      margin-top: 100rpx;
+      .select-item {
+        height: 120rpx;
+        background: #f8f9fb;
+        border-radius: 8rpx;
+        padding-left: 34rpx;
+        padding-right: 42rpx;
+        display: flex;
+        align-items: center;
+
+        .left {
+          width: 90%;
+          display: flex;
+          align-items: center;
+          margin-right: auto;
+
+          img {
+            width: 44rpx;
+            height: 40rpx;
+            margin-right: 16rpx;
+          }
+
+          span {
+            font-size: 32rpx;
+            font-weight: bold;
+            color: #383846;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            display: inline-block;
+            width: calc(100% - 60rpx);
+          }
+        }
+
+        &-hover {
+          background: #f2f2f3;
+        }
+      }
     }
   }
 }
 .content {
-  margin-top: 40rpx;
   padding: 0 30rpx;
   box-sizing: border-box;
 
-  .order-form-wrap {
+  .btn-history {
+    margin-top: 80rpx;
     width: 100%;
-    background: #fff;
-    border-radius: 68rpx;
-    padding-top: 46rpx;
-    padding-left: 44rpx;
-    padding-right: 44rpx;
-    padding-bottom: 40rpx;
-    box-sizing: border-box;
+    height: 120rpx;
+    border-radius: 12rpx;
+    border: 2rpx solid #326cbc;
+    font-size: 36rpx;
+    font-weight: bold;
+    color: #326cbc;
+    line-height: 120rpx;
+    text-align: center;
 
-    .title {
-      display: flex;
-      align-items: center;
-      margin-bottom: 54rpx;
-
-      img {
-        width: 30rpx;
-        height: 36rpx;
-        margin-right: 16rpx;
-      }
-      span {
-        font-size: 32rpx;
-        font-weight: bold;
-        color: #383846;
-        line-height: 44rpx;
-      }
+    &-hover {
+      background: #ebf1f8;
     }
+  }
+}
 
-    .order-form-item {
-      height: 90rpx;
-      background: #f8f9fb;
-      border-radius: 8rpx;
-      padding-left: 34rpx;
-      padding-right: 42rpx;
-      margin-bottom: 20rpx;
-      display: flex;
-      align-items: center;
+.role-type {
+  width: 100%;
+  height: 120rpx;
+  background: linear-gradient(130deg, #6db2e7 0%, #306abb 100%);
+  border-radius: 12rpx;
+  margin-top: 80rpx;
+  display: flex;
+  align-items: center;
 
-      .left {
-        width: 90%;
-        display: flex;
-        align-items: center;
-        margin-right: auto;
+  &-hover {
+    background: linear-gradient(130deg, #51a1df 0%, #2260b6 100%);
+  }
 
-        img {
-          width: 44rpx;
-          height: 40rpx;
-          margin-right: 16rpx;
-        }
+  img.staff {
+    width: 58rpx;
+    height: 44rpx;
+    margin-left: 76rpx;
+  }
 
-        span {
-          font-size: 26rpx;
-          font-weight: bold;
-          color: #383846;
-          white-space: nowrap;
-          text-overflow: ellipsis;
-          overflow: hidden;
-          display: inline-block;
-          width: calc(100% - 60rpx);
-        }
-      }
+  img {
+    width: 40rpx;
+    height: 48rpx;
+    margin-left: 92rpx;
+  }
 
-      &-hover {
-        background: #f2f2f3;
-      }
-    }
-
-    .route {
-      margin-top: 80rpx;
-      &-header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-
-        .left {
-          font-size: 28rpx;
-          font-weight: bold;
-          color: #383846;
-        }
-
-        .right {
-          font-size: 24rpx;
-          font-weight: 400;
-          color: #868686;
-          display: flex;
-          align-items: center;
-        }
-      }
-
-      &-content {
-        margin-top: 80rpx;
-        display: flex;
-        border-top: 2rpx solid #ccc;
-
-        &_layer {
-          width: 33.333%;
-          padding: 20rpx;
-          box-sizing: border-box;
-          position: relative;
-          text-align: center;
-          font-size: 24rpx;
-          font-weight: 400;
-          color: #383846;
-
-          &::before {
-            content: "";
-            position: absolute;
-            top: -10rpx;
-            left: 50%;
-            transform: translateX(-50%);
-            width: 20rpx;
-            height: 20rpx;
-            background: #ccc;
-            border-radius: 50%;
-          }
-
-          &.isArrived {
-            &::before {
-              background: #158edc;
-            }
-            &::after {
-              content: "";
-              position: absolute;
-              top: -2rpx;
-              left: 0;
-              width: 100%;
-              height: 4rpx;
-              background: #158edc;
-            }
-          }
-          &.isIn {
-            &::before {
-              background: #158edc;
-            }
-            &::after {
-              content: "";
-              position: absolute;
-              top: -2rpx;
-              left: 0;
-              width: 50%;
-              height: 4rpx;
-              background: #158edc;
-            }
-          }
-        }
-      }
-    }
+  span {
+    font-size: 36rpx;
+    font-weight: bold;
+    color: #ffffff;
+    margin-left: 128rpx;
   }
 }
 </style>
 
 <template>
   <app-layout>
-    <div class="header" :style="{ paddingTop: menuButtonInfo.top + 'px' }">
+    <div class="header" :style="{ paddingTop: headerPaddingTop + 'px' }">
       <div class="header-inner">
-        <div class="banner">
-          <u-swiper :list="banners" height="338" />
+        <div class="header-inner_title">
+          <p>乘车数据</p>
+          <p>报告平台</p>
         </div>
-      </div>
-    </div>
-    <div class="content">
-      <div class="order-form-wrap">
-        <div class="title">
-          <img src="../../static/images/home/icon-title.png" alt="" />
-          <span>查看路线</span>
-        </div>
-        <div class="order-form-inner">
+        <div class="select-wrap">
           <div
-            class="order-form-item"
-            hover-class="order-form-item-hover"
+            class="select-item"
+            hover-class="select-item-hover"
             hover-stay-time="150"
-            @click="onFormItemClick(item)"
           >
             <div class="left">
-              <span>
-                {{
-                  currentRoute.id
-                    ? `${currentRoute.code}（${currentRoute.name}）`
-                    : "请选择路线"
-                }}
-              </span>
+              <span> 请选择班次 </span>
             </div>
             <u-icon
               color="#000"
@@ -220,34 +162,39 @@
             />
           </div>
         </div>
-        <div class="route" v-if="currentRoute.id">
-          <div class="route-header">
-            <div class="left">途径路线：</div>
-            <div class="right" v-if="appUser.id" @click="onNavTo">
-              <span>查看详细站点</span>
-              <u-icon name="arrow-right" color="#868686" size="28" />
-            </div>
-            <div class="right" v-else open-type="getUserInfo" @click="login">
-              <span>查看详细站点</span>
-              <u-icon name="arrow-right" color="#868686" size="28" />
-            </div>
-          </div>
-          <div class="route-content">
-            <div
-              v-for="(s, i) in stations"
-              :key="i"
-              :class="[
-                'route-content_layer',
-                i < 1 ? 'isArrived' : '',
-                i === 1 ? 'isIn' : '',
-              ]"
-            >
-              <span>{{ s.name }}</span>
-            </div>
-          </div>
+        <div
+          class="role-type"
+          hover-class="role-type-hover"
+          hover-stay-time="150"
+        >
+          <img
+            class="staff"
+            src="../../static/images/home/icon-staff.png"
+            alt=""
+          />
+          <span>员工班车</span>
         </div>
       </div>
     </div>
+    <div class="content">
+      <div
+        class="role-type"
+        hover-class="role-type-hover"
+        hover-stay-time="150"
+      >
+        <img src="../../static/images/home/icon-tourist.png" alt="" />
+        <span>游客巴士</span>
+      </div>
+
+      <div
+        class="btn-history"
+        hover-class="btn-history-hover"
+        hover-stay-time="150"
+      >
+        查看历史数据
+      </div>
+    </div>
+
     <BindMobile
       v-model="bindMobileModal.visible"
       :userProfile="bindMobileModal.userProfile"
@@ -280,6 +227,12 @@ export default {
       currentRoute: {},
       stations: [],
     };
+  },
+
+  computed: {
+    headerPaddingTop() {
+      return this.menuButtonInfo.top + this.menuButtonInfo.height + 30;
+    },
   },
 
   watch: {
